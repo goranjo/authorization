4c4f5b800f41d47af9486760559a5ae661518caa
Steve Bauman <steven_bauman@outlook.com>
2016-02-21 23:28:05 -0500

    Fix middlewares

--- src/Middleware/PermissionMiddleware.php
+++ src/Middleware/PermissionMiddleware.php
@@ -29,7 +29,7 @@ class PermissionMiddleware
 
         $permissions = collect($permissions);
 
-        if (! $request->user()->hasPermission($permissions)) {
+        if (! $request->user()->hasPermissions($permissions)) {
             throw new UnauthorizedException();
         }
 
