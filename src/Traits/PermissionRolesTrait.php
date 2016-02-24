eed5196922b0edd490e3f1a5d950ecf0acd541e5
Steve Bauman <steven_bauman@outlook.com>
2016-02-24 14:26:45 -0500

    Added missing users relationship in permissions

--- src/Traits/PermissionRolesTrait.php
+++ src/Traits/PermissionRolesTrait.php
@@ -5,4 +5,16 @@ namespace Stevebauman\Authorization\Traits;
 trait PermissionRolesTrait
 {
     use HasRolesTrait;
+
+    /**
+     * A permission may have many users.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        $model = config('authorization.user');
+
+        return $this->belongsToMany($model);
+    }
 }
